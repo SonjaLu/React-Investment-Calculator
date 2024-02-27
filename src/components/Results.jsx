f38b8eee74877963ee5c50003c949ddd262ca646
SonjaLu <s_lueg@yahoo.com>
2024-02-27 21:49:14 +0100

    adding functionality

---
+++ src/components/Results.jsx
@@ -0,0 +1,8 @@
+import { calculateInvestmentResults } from '../util/investment.js';
+
+export default function Results({ userInput }) {
+    const resultsData = calculateInvestmentResults(userInput);
+
+    console.log(resultsData);
+    return <p>Results</p>
+}
